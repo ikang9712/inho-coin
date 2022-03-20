5ca4a6aed993c85b251ee20ec152ecb52cf21747
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-03-19 22:50:08 -0400

    made explorer file to clear main.go file

--- main.go
+++ main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/nomadcoders/nomadcoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(rw, "add", nil)
-}
+import "github.com/nomadcoders/nomadcoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
