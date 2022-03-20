b3a56380a4464bb1b36f7fdd4fe8a00749d02650
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-03-19 23:14:51 -0400

    implemented json.marshal

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/nomadcoders/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nomadcoders/nomadcoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Fprintf(rw, "%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
